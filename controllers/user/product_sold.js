5aa944acecaa352234c0acae6069bb4c07d5c2f7
Kelvin-Cendra <kelvin.cendra@fazzfinancial.com>
2022-07-11 18:40:18 +0700

    refactor api users

--- controllers/user/product_sold.js
+++ controllers/user/product_sold.js
@@ -5,17 +5,17 @@ const { Success200 } = require("../../helpers/response/success");
 const { Error500 } = require("../../helpers/response/error");
 
 async function soldProduct(req, res) {
-    try {
-        let header = req.headers.authorization.split("Bearer ")[1];
-        let user = jwt.verify(header, "s3cr3t");
+    // try {
+    //     let header = req.headers.authorization.split("Bearer ")[1];
+    //     let user = jwt.verify(header, "s3cr3t");
     
-        const productsList = await Products.findAll({
-          where: { user_id: user.id, status: SOLD_PRODUCT },
-        });
-        return Success200(res, productsList);
-      } catch (err) {
-        return Error500(res, err.message);
-      }
+    //     const productsList = await Products.findAll({
+    //       where: { user_id: user.id, status: SOLD_PRODUCT },
+    //     });
+    //     return Success200(res, productsList);
+    //   } catch (err) {
+    //     return Error500(res, err.message);
+    //   }
     }
 
 module.exports = soldProduct;
