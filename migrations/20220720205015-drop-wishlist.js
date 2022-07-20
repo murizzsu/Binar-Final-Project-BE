821d91344a25cae5ee42300f67632c49ae918aa6
mardhaniDN <mardhani9497fmipa.2019@student.uny.ac.id>
2022-07-20 21:33:12 +0700

    delete wishlist and update testing register

---
+++ migrations/20220720205015-drop-wishlist.js
@@ -0,0 +1,6 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.dropTable('wishlist');
+  },
+};
--- test/user/register.spec.js
+++ test/user/register.spec.js
@@ -1,39 +1,71 @@
 const register = require("../../controllers/user/register");
 const express = require("express");
 const request = require("supertest");
+const { Users } = require("../../models");
 
 jest.setTimeout(20000);
 const app = express();
 app.use(express.json())
 app.post("/api/v1/register", register)
 
+const newUser = {
+    name: "Wafi",
+    email: "wafi@gmail.com",
+    password: "12345",
+}
+
+const registeredUser = {
+    name: "Rizky",
+    email: "rizky@gmail.com",
+    password: "12345"
+}
+
 describe("Register POST /api/v1/register", () => {
     describe("Register success", () => {
-        beforeAll(() => {
-
+        beforeAll(async () => {
+            await Users.create(registeredUser);
         })
 
-        afterAll(() => {
-
+        afterAll(async () => {
+            await Users.destroy({
+                where: {
+                  name: newUser.name,
+                  email: newUser.email,
+                },
+              });
+        
         })
 
-        it("Return status code 200, get data", async () => {
-            const newUser = {
-                name: "Dhani",
-                email: "dhani@gmail.com",
-                password: "12345",
-            }
+        it("Return status code 200, get data", (done) => {
 
-            const response = await request(app)
-                .post("/api/v1/register")
-                .set("Content-Type", "application/json")
-                .send(newUser)
-                .expect(200)
-            expect(response.body).toBeTruthy();
+            request(app)
+            .post("/api/v1/register")
+            .set("Content-Type", "application/json")
+            .send(newUser)
+            .expect(200)
+            .then((res) => {
+              expect(res.body).toBeTruthy();
+              done();
+            })
+            .catch(done);
+
         })
     })
 
     describe("Register unsuccess", () => {
 
+        it("User Already Exist", (done) => {
+
+            request(app)
+            .post("/api/v1/register")
+            .set("Content-Type", "application/json")
+            .send(registeredUser)
+            .expect(400)
+            .then((res) => {
+              expect(res.body.message).toBe("User Already Exist");
+              done();
+            })
+            .catch(done);
+        })
     })
 })
